98677be2f1d087250b366d6e197f693d832ea3a8
Username <Email-Use-Github-Anonymous>
2021-07-25 12:41:33 +0100

    made gui

---
+++ src/GUI.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class GUI extends JFrame {
+    public GUI(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+}
