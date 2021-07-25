a7705b9e40a567a92616d0b0b951a417854132a1
Username <Email-Use-Github-Anonymous>
2021-07-25 13:22:12 +0100

    made a basic table

--- src/GUI.java
+++ src/GUI.java
@@ -6,7 +6,28 @@ import java.io.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class GUI extends JFrame {
+    private JPanel dataTablePanel;
+    private JTable dataTable;
+    private JScrollPane dataTableScrollPane;
+
     public GUI(){
+        super("Translate");
+        createGUI();
+        finalizeGUI();
+    }
+
+    private void createGUI() {
+        dataTablePanel = new JPanel();
+        String[][] data = {{"hi1", "hi1"}, {"hi2", "hi2"}};
+        String[] columnNames = {"English", "Swedish"};
+        dataTable = new JTable(data, columnNames);
+        dataTableScrollPane = new JScrollPane(dataTable);
+        dataTableScrollPane.setPreferredSize(new Dimension(1000, 600));
+        dataTablePanel.add(dataTableScrollPane);
+    }
+
+    private void finalizeGUI() {
+        add(dataTablePanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
