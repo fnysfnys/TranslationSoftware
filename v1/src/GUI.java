3909c3159766d515b98ea427b04bb1938c85916e
fnysfnys <gabrielturner3301@gmail.com>
2021-08-01 18:44:56 +0100

    added a v1 dir for making newer, cleaner versions.

---
+++ v1/src/GUI.java
@@ -0,0 +1,300 @@
+import java.awt.*;
+import javax.swing.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
+public class GUI extends JFrame {
+    private final JPanel dataViewPanel;
+    private JPanel dataTablePanel;
+    private JTable dataTable;
+    private JScrollPane dataTableScrollPane;
+    private JFileChooser fc;
+    private String[] columnNames = {"English", "Swedish"};
+    private String[][] data;
+    private Model model;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JPanel infoDisplayPanel;
+    private JLabel lastSaveLabel;
+    private JLabel autoSaveIndicationLabel;
+    private JLabel memoryLabel;
+
+    public GUI(){
+        super("Translation Project (NOT SAVED)");
+        model = new Model();
+        dataViewPanel = new JPanel(new BorderLayout());
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        createFileChooser();
+        createGUI();
+        finalizeGUI();
+    }
+
+    private void createFileChooser() {
+        fc = new JFileChooser(".");
+        fc.setAcceptAllFileFilterUsed(false);
+    }
+
+    private void attemptFileLoad(){
+        try{
+            loadFile();
+        }
+        catch (Exception exp){
+            displayErrorMessage("Unable to load file into GUI: Improperly formatted or Contains no data.",
+                    "File Error");
+            attemptFileLoad();
+        }
+    }
+
+    private void displayErrorMessage(String message, String title) {
+        JOptionPane.showMessageDialog(this, message, title, JOptionPane.ERROR_MESSAGE);
+    }
+
+    private void loadFile(){
+        int returnVal = fc.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION){
+            File file = fc.getSelectedFile();
+            String filePath = file.getPath();
+            model.loadDataFromDoc(filePath);
+            data = model.getData();
+        }
+        else {
+            //handle no file chosen
+        }
+        updateFrame();
+    }
+
+    private void updateFrame() {
+        dataTablePanel.remove(dataTableScrollPane);
+        createTableModel();
+        dataTable.setAutoCreateRowSorter(true);
+        dataTableScrollPane = new JScrollPane(dataTable);
+
+        dataTableScrollPane.setPreferredSize(new Dimension(1920, 1080));
+        dataTablePanel.add(dataTableScrollPane);
+
+        addAutoSaveActionListener();
+
+        dataTablePanel.revalidate();
+        dataTablePanel.validate();
+        dataTablePanel.repaint();
+    }
+
+    private void addAutoSaveActionListener() {
+        dataTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent event) {
+                if (dataTable.getSelectedRow() > -1) {
+                    model.getNonSavedValuesFromTable(dataTable);
+
+                    if(model.memoryLoaded()){
+                        model.saveMemory(dataTable);
+                    }
+                    if(model.autoSavable()){
+                        model.autoSave(dataTable);
+                        updateSaveDate();
+                        System.out.println("auto saved."); //test code
+                    }
+                }
+            }
+        });
+    }
+
+    private void createTableModel() {
+        DefaultTableModel dm = new DefaultTableModel() {
+            public Class<String> getColumnClass(int columnIndex) {
+                return String.class;
+            }
+            public boolean isCellEditable(int row, int column) {
+                return true;
+            }
+        };
+
+        dm.setDataVector(data, new String[]{"From", "To"});
+        dataTable = new JTable(dm);
+
+        dataTable.setDefaultRenderer(String.class, new MultiLineTableCellRenderer());
+        addAutoSaveActionListener();
+    }
+
+    private void createGUI() {
+        createMenu();
+        createTable();
+        createInfoDisplay();
+    }
+
+    private void createInfoDisplay() {
+        infoDisplayPanel = new JPanel(new BorderLayout());
+        lastSaveLabel = new JLabel("    Last Saved: --");
+        autoSaveIndicationLabel = new JLabel("Auto Save: OFF", SwingConstants.CENTER);
+        memoryLabel = new JLabel("Active Translation Memory: --    ");
+        infoDisplayPanel.add(lastSaveLabel, BorderLayout.WEST);
+        infoDisplayPanel.add(autoSaveIndicationLabel, BorderLayout.CENTER);
+        infoDisplayPanel.add(memoryLabel, BorderLayout.EAST);
+        dataViewPanel.add(infoDisplayPanel, BorderLayout.SOUTH);
+    }
+
+    private String getCurrentDateTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+
+    private void createMenu(){
+        menuBar = new JMenuBar();
+        createFileMenu();
+        this.setJMenuBar(menuBar);
+    }
+
+    private void createFileMenu() {
+        fileMenu = new JMenu("File");
+        createNewMenu();
+        createLoadMenu();
+        createSaveAsMenu();
+        createExportMenu();
+        menuBar.add(fileMenu);
+    }
+
+    private void createNewMenu() {
+        JMenu newMenu = new JMenu("new...");
+        JMenuItem newDocumentItem = new JMenuItem("new document");
+        JMenuItem newTranslationMemoryItem = new JMenuItem("new translation memory");
+
+        newMenu.add(newTranslationMemoryItem);
+        newMenu.add(newDocumentItem);
+
+        newDocumentItem.addActionListener((ActionEvent e) -> loadDocument());
+        newTranslationMemoryItem.addActionListener((ActionEvent e) -> createNewMemory());
+
+        fileMenu.add(newMenu);
+    }
+
+    private void createNewMemory() {
+        JFileChooser fc = new JFileChooser(".");
+        fc.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter csvFilter = new FileNameExtensionFilter("CSV Files", "csv");
+        fc.addChoosableFileFilter(csvFilter);
+        int returnVal = fc.showSaveDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            String newMemoryFilePath = file.getPath();
+            model.createMemory(newMemoryFilePath);
+            memoryLabel.setText("Active Translation Memory: " + newMemoryFilePath + "    ");
+        }
+    }
+
+    private void createLoadMenu() {
+        JMenu loadMenu = new JMenu("Load...");
+        JMenuItem savedProjectItem = new JMenuItem("saved project");
+        JMenuItem loadMemoryItem = new JMenuItem("translation memory");
+
+        loadMenu.add(savedProjectItem);
+        loadMenu.add(loadMemoryItem);
+
+        savedProjectItem.addActionListener((ActionEvent e) -> loadProject());
+        loadMemoryItem.addActionListener((ActionEvent e) -> loadMemory());
+
+        fileMenu.add(loadMenu);
+    }
+
+    private void loadDocument() {
+        attemptFileLoad();
+    }
+
+    private void loadProject() {
+        int returnVal = fc.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION){
+            File file = fc.getSelectedFile();
+            String filePath = file.getPath();
+            model.loadDataFromProj(filePath);
+            data = model.getData();
+            autoSaveIndicationLabel.setText("Auto Save: ON");
+            updateSaveDate();
+            this.setTitle(filePath);
+        }
+        else{
+            //handle no file chosen
+        }
+        updateFrame();
+    }
+
+    private void loadMemory() {
+        int returnVal = fc.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION){
+            File file = fc.getSelectedFile();
+            String filePath = file.getPath();
+            model.loadMemory(filePath, dataTable);
+            memoryLabel.setText("Active Translation Memory: " + filePath + "    ");
+        }
+        else{
+            //handle no file chosen
+        }
+    }
+
+    private void createExportMenu() {
+        JMenu saveAsMenu = new JMenu("Export as...");
+        JMenuItem exportAsDocxItem = new JMenuItem("docx file");
+
+        saveAsMenu.add(exportAsDocxItem);
+        exportAsDocxItem.addActionListener((ActionEvent e) -> exportAsDocx());
+        fileMenu.add(saveAsMenu);
+    }
+
+    private void exportAsDocx() {
+        System.out.println("exported");
+    }
+
+    private void createSaveAsMenu() {
+        JMenu saveAsMenu = new JMenu("Save As...");
+        JMenuItem writeToJSONItem = new JMenuItem("Translation Project");
+
+        saveAsMenu.add(writeToJSONItem);
+        writeToJSONItem.addActionListener((ActionEvent e) -> saveAsTranslationProject());
+        fileMenu.add(saveAsMenu);
+    }
+
+    private void saveAsTranslationProject() {
+        JFileChooser fc = new JFileChooser(".");
+        fc.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter csvFilter = new FileNameExtensionFilter("TMP Files", "tmp");
+        fc.addChoosableFileFilter(csvFilter);
+        String filePathToSave;
+        int returnVal = fc.showSaveDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            filePathToSave = file.getPath();
+            model.saveProject(filePathToSave, dataTable);
+            autoSaveIndicationLabel.setText("Auto Save: ON");
+            updateSaveDate();
+            this.setTitle(filePathToSave);
+        }
+    }
+
+    private void updateSaveDate() {
+        lastSaveLabel.setText("    Last Save: " + getCurrentDateTime());
+    }
+
+    private void createTable() {
+        dataTablePanel = new JPanel();
+
+        createTableModel();
+        dataTableScrollPane = new JScrollPane(dataTable);
+        dataTableScrollPane.setPreferredSize(new Dimension(1920, 1080));
+        dataTablePanel.add(dataTableScrollPane);
+        dataViewPanel.add(dataTablePanel, BorderLayout.CENTER);
+    }
+
+    private void finalizeGUI() {
+        this.add(dataViewPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+}
---
+++ v1/src/Model.java
@@ -0,0 +1,162 @@
+import javax.swing.*;
+import java.io.*;
+
+public class Model {
+
+    private DataFrame dataFrame;
+
+    private TranslationMemory translationMemory;
+
+    public Model() {
+        dataFrame = new DataFrame();
+    }
+
+    public void loadDataFromDoc(String fileName) {
+        dataFrame = new DataFrame();
+        DataLoader dataLoader = new DataLoader(fileName);
+        dataLoader.loadDataToFrame(dataFrame);
+    }
+
+    public String[][] getData() {
+        int rowCount = dataFrame.getRowCount();
+        String[][] data = new String[rowCount][2];
+        Row currentRow;
+        for (int row = 0; row < rowCount; row++) {
+            currentRow = dataFrame.getRow(row);
+            data[row][0] = currentRow.getFromValue();
+            data[row][1] = currentRow.getToValue();
+        }
+
+        return data;
+    }
+
+    public boolean memoryLoaded(){
+        return this.translationMemory != null;
+    }
+
+    public void saveMemory(JTable dataTable) {
+        fillTranslationMemory(dataTable);
+        try {
+            WriteTranslationMemory();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void saveProject(String filePathToSave, JTable dataTable) {
+        try {
+            WriteDataFrame(filePathToSave);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void fillTranslationMemory(JTable dataTable) {
+        int rowCount = dataFrame.getRowCount();
+
+        for (int row = 0; row < rowCount; row++) {
+            if(!translationMemory.inMemory((String) dataTable.getValueAt(row, 0))){
+                translationMemory.addTranslation((String) dataTable.getValueAt(row, 0), (String) dataTable.getValueAt(row, 1));
+            }
+        }
+    }
+
+    private void WriteTranslationMemory() throws IOException {
+        FileOutputStream fos = new FileOutputStream(this.translationMemory.getFilePath());
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(this.translationMemory);
+
+        oos.close();
+    }
+
+    private void WriteDataFrame(String filePathToSave) throws IOException {
+        FileOutputStream fos = new FileOutputStream(filePathToSave);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        this.dataFrame.setProjectFilePath(filePathToSave);
+
+        oos.writeObject(this.dataFrame);
+
+        oos.close();
+    }
+
+    public void getNonSavedValuesFromTable(JTable dataTable) {
+        int rowCount = dataFrame.getRowCount();
+        String currentValue;
+        Row currentRow;
+        for (int row = 0; row < rowCount; row++) {
+            currentRow = dataFrame.getRow(row);
+            currentRow.setToValue(((String) dataTable.getValueAt(row, 1)));
+        }
+    }
+
+    public void loadDataFromProj(String filePath) {
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            this.dataFrame = (DataFrame) ois.readObject();
+
+            ois.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void createMemory(String newMemoryFilePath) {
+        this.translationMemory = new TranslationMemory(newMemoryFilePath);
+    }
+
+    public boolean autoSavable() {
+        return dataFrame.autoSavable();
+    }
+
+    public void autoSave(JTable dataTable) {
+        getNonSavedValuesFromTable(dataTable);
+
+        try {
+            WriteDataFrame(dataFrame.getProjectFilePath());
+            WriteTranslationMemory();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadMemory(String filePath, JTable dataTable) {
+        loadMemoryToModel(filePath);
+        loadMemoryToFrame(filePath, dataTable);
+    }
+
+    private void loadMemoryToFrame(String filePath, JTable dataTable) {
+        dataFrame.setMemoryFilePath(filePath);
+        int rowCount = dataFrame.getRowCount();
+        Row currentRow;
+        for (int row = 0; row < rowCount; row++) {
+            currentRow = dataFrame.getRow(row);
+            if(translationMemory.inMemory(currentRow.getFromValue())){
+                currentRow.setToValue(translationMemory.getTranslation(currentRow.getFromValue()));
+                dataTable.setValueAt(translationMemory.getTranslation(currentRow.getFromValue()), row, 1);
+            }
+        }
+    }
+
+    private void loadMemoryToModel(String filePath) {
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            this.translationMemory = (TranslationMemory) ois.readObject();
+
+            ois.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
